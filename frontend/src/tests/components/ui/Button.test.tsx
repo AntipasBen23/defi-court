878d8ffb3364523948b7dc4bff50739ddee4aa99
Antipas Ben <benantipas@gmail.com>
2025-07-10 00:12:21 +0100

    resolved tests erros

--- frontend/src/tests/components/ui/Button.test.tsx
+++ frontend/src/tests/components/ui/Button.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react'
-import Button from '@/components/ui/Button'
+import { Button } from '@/components/ui/Button'
 
 // Group related tests together
 describe('Button Component', () => {
